1c64c64b6c42105f9e3d20544474344bc96023da
Zsend <zakk.jensen@gmail.com>
2026-06-22 03:21:15 -0600

    as

--- assets/init.js
+++
@@ -1,6 +0,0 @@
-document.documentElement.classList.add("js");
-window.setTimeout(() => {
-  if (!document.documentElement.classList.contains("app-ready")) {
-    document.documentElement.classList.add("reveal-fallback");
-  }
-}, 2000);
